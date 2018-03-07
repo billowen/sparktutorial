f881807a66dc2283ef48c7170aca1ce0630ffa6c
KP Shao <kp.shao@outlook.com>
2018-03-07 18:26:33 +0800

    Commit simple sql sample

--- src/main/java/sql/SparkSQLExample.java
+++ src/main/java/sql/SparkSQLExample.java
@@ -3,6 +3,7 @@ package sql;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 public class SparkSQLExample {
     public static void main(String[] args) {
@@ -17,6 +18,15 @@ public class SparkSQLExample {
 
     private static void runBasicDataFrameExample(SparkSession spark) {
         Dataset<Row> df = spark.read().json("src/main/resources/people.json");
-        df.show();
+//        df.show();
+//        df.printSchema();
+//        df.select(col("name")).show();
+//        df.select(col("name"), col("age").plus(1)).show();
+//        df.filter(col("age").gt(21)).show();
+//        df.groupBy("age").count().show();
+
+        df.createOrReplaceTempView("people");
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
+        sqlDF.show();
     }
 }
